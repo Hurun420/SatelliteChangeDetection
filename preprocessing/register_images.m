07b1b032ae8429130f51aa016349be0f6d99514b
Run Hu <go25nog@mytum.de>
2025-07-09 17:13:02 +0200

    feat: first working version of UI app

--- preprocessing/register_images.m
+++ preprocessing/register_images.m
@@ -5,7 +5,12 @@ function [alignedImages, transformParams] = register_images(folderPath, imageLis
 
     % Read and convert to grayscale as the global reference image
     refImage = imread(fullfile(folderPath, imageList{1}));
-    refImageGray = rgb2gray(refImage);
+    if size(refImage,3) == 3
+        refImageGray = rgb2gray(refImage);
+    else
+        refImageGray = refImage;
+    end
+
     alignedImages{1} = refImageGray;
     transformParams{1} = affine2d(eye(3));
     
@@ -15,7 +20,12 @@ function [alignedImages, transformParams] = register_images(folderPath, imageLis
     for i = 2:numImages
         fprintf("⚙️ Registering image %d / %d: %s\n", i, numImages, imageList{i});
         curImage = imread(fullfile(folderPath, imageList{i}));
-        curImageGray = rgb2gray(curImage);
+        if size(curImage,3) == 3
+            curImageGray = rgb2gray(curImage);
+        else
+            curImageGray = curImage;
+        end
+
 
         % Detect feature points in the current image
         pts_cur = harris_detector(curImageGray, 'tau', 1e5, 'do_plot', false, 'segment_length', 15, 'max_features', 500, 'auto_adjust_tau', true);
