e937d04a2f86c69b8c2dc01cfc49a6e4079c9145
YapingH <go25wor@mytum.de>
2025-07-10 15:50:22 +0200

    modified plot matches

--- preprocessing/plot_matches.m
+++ preprocessing/plot_matches.m
@@ -1,18 +1,12 @@
-%% plot_matches.m
 function plot_matches(im1, im2, pts1, pts2, savePath)
-    % Create a color overlay of feature matches between two grayscale images
-    imCat = cat(2, im1, im2);
-    figure('Visible','off'); imshow(imCat); hold on;
-    offset = size(im1, 2);
-
-    % Draw points and lines
-    for i = 1:size(pts1, 2)
-        plot(pts1(1,i), pts1(2,i), 'ro', 'MarkerSize', 3, 'LineWidth', 1.5);
-        plot(pts2(1,i) + offset, pts2(2,i), 'go', 'MarkerSize', 3, 'LineWidth', 1.5);
-        line([pts1(1,i), pts2(1,i) + offset], [pts1(2,i), pts2(2,i)], 'Color', [0, 1, 0, 0.3]);
-    end
-
-    % Save result
-    saveas(gcf, savePath);
-    close;
+imCat = cat(2, im1, im2);
+figure('Visible','off'); imshow(imCat); hold on;
+offset = size(im1, 2);
+for i = 1:size(pts1, 2)
+    plot(pts1(1,i), pts1(2,i), 'ro', 'MarkerSize', 3);
+    plot(pts2(1,i) + offset, pts2(2,i), 'go', 'MarkerSize', 3);
+    line([pts1(1,i), pts2(1,i) + offset], [pts1(2,i), pts2(2,i)], ...
+        'Color', [0, 1, 0, 0.3]);
+end
+saveas(gcf, savePath); close;
 end
